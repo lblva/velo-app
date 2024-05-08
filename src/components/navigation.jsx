e982f5da93f6e4b57cd97df466e4b7f1acd08bd8
lblva <lola.batchalova@hotmail.com>
2024-05-08 15:45:50 +0200

    initial  commit. with header and nav

---
+++ src/components/navigation.jsx
@@ -0,0 +1,53 @@
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navigation() 
+{
+    return(
+        <>  <div className={styles.navigationContainer}> 
+                <nav className={styles.navigation}>
+                <Link href="/">
+                        <Image
+                        src="/homeIcon.png"
+                        alt="home Icon"
+                        className={styles.homeIcon}
+                        width={38}
+                        height={40}
+                        />
+                    </Link>
+                    
+                    <Link href="/">
+                        <Image
+                        src="/helpIcon.png"
+                        alt="help Icon"
+                        className={styles.helpIcon}
+                        width={38}
+                        height={40}
+                        />
+                    </Link>
+
+                    <Link href="/">
+                        <Image
+                        src="/accountIcon.png"
+                        alt="account Icon"
+                        className={styles.accountIcon}
+                        width={38}
+                        height={35}
+                        />
+                    </Link>
+
+                    <Link href="/">
+                        <Image
+                        src="/moreIcon.png"
+                        alt="more Icon"
+                        className={styles.moreIcon}
+                        width={38}
+                        height={40}
+                        />
+                    </Link>
+                </nav>
+            </div>
+        </>
+    );
+}
